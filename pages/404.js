6c88fe265338e890c9ced80281e47705d8da9854
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-09 18:38:05 +0100

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+function NotFount() {
+  return (
+    <Box display='flex' height='100vh' justifyContent='center' alignItems='center' flexDirection='column' gap='4'>
+      <Heading>
+        Oops... Seems you&apos;ve stumbled beyond the universe.
+      </Heading>
+      <Text fontSize='2xl'>Head back to one of the tabs using the navbar</Text>
+    </Box>
+  );
+}
+
+export default NotFount;
