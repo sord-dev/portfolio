8281c55e47c3a30df7cfd83fd10861b077727bc6
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-09 19:08:18 +0100

    +about

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,25 @@
 import { Box, Heading, Spacer, Text } from "@chakra-ui/react";
-import { Head } from "next/head";
+import Head from "next/head";
 import React from "react";
 import styles from "../styles/About.module.css";
 
 function about() {
   return (
     <>
+      <Head>
+        <title>Sord-dev&apos;s Portfolio - About</title>
+      </Head>
+      <Box as="div" className={styles.main}>
+        <Box as="div" className={styles.container}>
+
+        <Heading >This is going to be the about</Heading>
+        <Text as='p' mb={10}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque maxime eum earum beatae, iusto voluptatum similique rem magnam quidem officiis facere ipsam quisquam ducimus et laborum modi dolorem aliquid fugit!</Text>
+
+        <Heading >Skills</Heading>
+        <Text as='p'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque maxime eum earum beatae, iusto voluptatum similique rem magnam quidem officiis facere ipsam quisquam ducimus et laborum modi dolorem aliquid fugit!</Text>
       
+        </Box>
+      </Box>
     </>
   );
 }
