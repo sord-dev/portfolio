e2fee57bf4da33f4e80155aad155bc2ef6953234
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-09 16:46:41 +0100

    added routing animation fade in

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@chakra-ui/theme";
 import "../styles/globals.css";
 import Layout from "./components/Layout";
+import { motion } from 'framer-motion'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+    <motion.div key={router.route} initial='pageInitial' animate='pageAnimate' variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      }
+    }}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </motion.div>
     </ChakraProvider>
   );
 }
