daa4281ff7b8cf479009f93d69b485f94fe9655d
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-09 12:29:24 +0100

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@chakra-ui/theme'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
