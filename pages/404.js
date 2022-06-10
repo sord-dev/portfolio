a2242eacda6f2027149549b8fc49879f43c75dec
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-10 17:53:49 +0100

    404 text center

--- pages/404.js
+++ pages/404.js
@@ -4,10 +4,10 @@ import React from "react";
 function NotFount() {
   return (
     <Box display='flex' height='100vh' justifyContent='center' alignItems='center' flexDirection='column' gap='4'>
-      <Heading>
+      <Heading textAlign='center'>
         Oops... Seems you&apos;ve stumbled beyond the universe.
       </Heading>
-      <Text fontSize='2xl'>Head back to one of the tabs using the navbar</Text>
+      <Text textAlign='center' fontSize='2xl'>Head back to one of the tabs using the navbar</Text>
     </Box>
   );
 }
