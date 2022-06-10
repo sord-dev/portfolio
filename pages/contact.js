b26fee8481a88527ffd08f1af770af953a726247
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-10 15:04:38 +0100

    decided to revamp contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,20 +1,13 @@
 import {
   Box,
-  Button,
-  FormControl,
-  FormLabel,
   Heading,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Textarea,
-  VStack,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import React from "react";
 import styles from "../styles/Contact.module.css";
 
 function contact() {
+
   return (
     <>
       <Head>
@@ -26,50 +19,7 @@ function contact() {
             Get in touch
           </Heading>
 
-          <Box className={styles.card}>
-            <VStack spacing={5}>
-              <FormControl isRequired>
-                <FormLabel>Name</FormLabel>
-
-                <InputGroup>
-                  <InputLeftElement />
-                  <Input type="text" name="name" placeholder="Your Name" />
-                </InputGroup>
-              </FormControl>
-
-              <FormControl isRequired>
-                <FormLabel>Email</FormLabel>
-
-                <InputGroup>
-                  <InputLeftElement />
-                  <Input type="email" name="email" placeholder="Your Email" />
-                </InputGroup>
-              </FormControl>
-
-              <FormControl isRequired>
-                <FormLabel>Message</FormLabel>
-
-                <Textarea
-                  name="message"
-                  placeholder="Your Message"
-                  rows={6}
-                  resize="none"
-                />
-              </FormControl>
-
-              <Button
-                colorScheme="blue"
-                bg="blue.400"
-                color="white"
-                _hover={{
-                  bg: "blue.500",
-                }}
-                isFullWidth
-              >
-                Send Message
-              </Button>
-            </VStack>
-          </Box>
+          
         </Box>
       </Box>
     </>
