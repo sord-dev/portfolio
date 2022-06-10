eb08326102151ca50da6f740a4a1eb16a4c1b3f0
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-10 16:24:46 +0100

    +github section

--- pages/components/GithubTab.js
+++ pages/components/GithubTab.js
@@ -1,7 +1,42 @@
+import {
+  Avatar,
+  Box,
+  Heading,
+  Link,
+  Stack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
-function GithubTab() {
-  return <div>GithubTab</div>;
+function GithubTab({ data }) {
+  return (
+    <Box p={5}>
+
+    <Heading mb={5} size={'md'}>
+        Check me out on github
+    </Heading>
+
+      <Link href={data.html_url}>
+        <Avatar src={data.avatar_url} size="lg" mb={5} />
+        <Text>{`@${data.login}`}</Text>
+      </Link>
+
+      <Stack direction={"row"} p={1} m={1}>
+        <Stat>
+          <StatLabel>public repos</StatLabel>
+          <StatNumber>{data.public_repos}</StatNumber>
+        </Stat>
+
+        <Stat>
+          <StatLabel>followers</StatLabel>
+          <StatNumber>{data.followers}</StatNumber>
+        </Stat>
+      </Stack>
+    </Box>
+  );
 }
 
 export default GithubTab;
