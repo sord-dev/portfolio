9a09fe8183984a60b9a7b76adc09ccb0fa682689
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-06-11 10:47:57 +0100

    added working linkedin link

--- pages/components/GithubTab.js
+++ pages/components/GithubTab.js
@@ -8,21 +8,23 @@ import {
   StatLabel,
   StatNumber,
   Text,
+  Tooltip,
 } from "@chakra-ui/react";
 import React from "react";
 
 function GithubTab({ data }) {
   return (
     <Box p={5}>
-
-    <Heading mb={5} size={'md'}>
+      <Heading mb={5} size={"md"}>
         Check me out on github
-    </Heading>
+      </Heading>
 
-      <Link href={data.html_url}>
-        <Avatar src={data.avatar_url} size="lg" mb={5} />
-        <Text>{`@${data.login}`}</Text>
-      </Link>
+      <Tooltip label='To Github'>
+        <Link href={data.html_url}>
+          <Avatar src={data.avatar_url} size="lg" mb={5} />
+          <Text>{`@${data.login}`}</Text>
+        </Link>
+      </Tooltip>
 
       <Stack direction={"row"} p={1} m={1}>
         <Stat>
--- pages/components/LinkedInTab.js
+++ pages/components/LinkedInTab.js
@@ -1,19 +1,37 @@
-import { Box, Heading, } from "@chakra-ui/react";
+import { Box, Heading, Link, Text, Tooltip } from "@chakra-ui/react";
 import React from "react";
+import { IconContext } from "react-icons";
+import { AiFillLinkedin } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 function LinkedInTab() {
   return (
     <Box p={5}>
+      <Box mb={5} textAlign="center">
+        <Heading size={"md"} pb='5'>Check out my LinkedIn</Heading>
 
-    <Heading mb={5} size={'md'}>
-        Check me out on linkedin
-    </Heading>
+        <Tooltip label="To LinkedIn">
+          <Link
+            as={motion.a}
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.2 },
+            }}
+            href="https://www.linkedin.com/in/stefan-syrett-558380223/"
+            display="flex"
+            flexDirection='column'
+            alignItems='center'
+            justifyContent="center"
+          >
+            <AiFillLinkedin size={70} />
 
-    
-
-
+            <Text>Stefan Syrett</Text>
+            <Text>London, England, United Kingdom</Text>
+          </Link>
+        </Tooltip>
+      </Box>
     </Box>
-  )
+  );
 }
 
 export default LinkedInTab;
